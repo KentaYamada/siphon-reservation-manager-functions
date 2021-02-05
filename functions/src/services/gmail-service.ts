961dbbc205057469385355c385cf01b7ae42deca
Kenta Yamada <knt.yamada.800@gmail.com>
2021-02-06 00:09:37 +0900

    Create Gmail service (#1)

---
+++ functions/src/services/gmail-service.ts
@@ -0,0 +1,40 @@
+import { config } from "firebase-functions";
+import * as functions from "firebase-functions";
+import * as nodemailer from "nodemailer";
+import * as Mail from "nodemailer/lib/mailer";
+import * as SMTPConnection from "nodemailer/lib/smtp-connection";
+
+/**
+ * Gmail service
+ */
+export class GmailService {
+  // Cloud functions env
+  private static readonly config: config.Config = functions.config();
+
+  /**
+   * Gmail configurations
+   */
+  static get gmailConfig() {
+    return GmailService.config.gmail;
+  }
+
+  /**
+   * Send email
+   * @param {Mail.Options} transmissionData
+   * @returns {Promise<any>}
+   */
+  static send(transmissionData: Mail.Options): Promise<any> {
+    const connection: SMTPConnection.Options = {
+      host: "smtp.gmail.com",
+      port: 465,
+      secure: true,
+      auth: {
+        user: GmailService.config.gmail.email,
+        pass: GmailService.config.gmail.password,
+      },
+    };
+    const transport = nodemailer.createTransport(connection);
+
+    return transport.sendMail(transmissionData);
+  }
+}
